91de0f41a49f75a6cb7529e98695a56f9a5131eb
hyun <caudeutsch@gmail.com>
2023-05-19 19:32:08 +0900

    Initial commit

---
+++ src/main/java/com/animal/animalhood/AnimalhoodApplication.java
@@ -0,0 +1,13 @@
+package com.animal.animalhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalhoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animal/animalhood/AnimalhoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animal.animalhood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalhoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
